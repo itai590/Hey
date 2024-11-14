d8ff5853466a62c7b9018fc1de95e060490d8e05
Itai Koren <34794316+itai590@users.noreply.github.com>
2024-11-14 13:25:20 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,10 +1,9 @@
 export default {
-    apiKey: "AIzaSyAv_iqx8Kri7G__NX4R9qBOmkyBwMTAEfs",
-    authDomain: "hey-x.firebaseapp.com",
-    databaseURL: "https://hey-x.firebaseio.com",
-    projectId: "hey--x",
-    storageBucket: "hey--x.appspot.com",
-    messagingSenderId: "153383323733",
-    appId: "1:153383323733:web:de8a615e58f74eaf",
-    measurementId: "G-HRJR0RSJ28"
+  apiKey: "AIzaSyB3Vbxs5qEMKQ6ISy2GR_r61VgPxCyHtbA",
+  authDomain: "hey-sheldon-410513.firebaseapp.com",
+  projectId: "hey-sheldon-410513",
+  storageBucket: "hey-sheldon-410513.firebasestorage.app",
+  messagingSenderId: "552625390174",
+  appId: "1:552625390174:web:6f4c0b2831ced261cbaacf",
+  measurementId: "G-9SEKQR97L4"
 }
