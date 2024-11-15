d5b03bedd5adfddbbc25e19295d2f857bd3cf620
Itai Koren <itai.koren@psifas.org.il>
2024-11-15 12:07:50 +0200

    refactor(functions): update Firebase functions to modular SDK and upgrade dependencies
    Updated the Firebase functions to use the modular SDK in index.js. Removed old Firestore triggers and added sample HTTPS function. Upgraded Node.js version to 18 and updated dependencies and devDependencies in package.json. Modified .gitignore to include *.local files.

--- functions/index.js
+++ functions/index.js
@@ -1,90 +1,19 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-try {
-    admin.initializeApp(functions.config().firebase);
-} 
-catch (e) {
-    console.error('Caught error initializing app with functions.config():', e.message || e);
-}
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-const firestore = admin.firestore()
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
 // });
-
-exports.hub = functions.firestore
-    .document('/hubs/{hubId}/{col}/{messageId}')
-    .onCreate(collectionCreate(({ hubId }) => `/hubs/${hubId}`))
-
-exports.hubSubCollection = functions.firestore
-    .document('/hubs/{hubId}/{colParent}/{docId}/{col}/{messageId}')
-    .onCreate(collectionCreate(({ hubId, colParent, docId }) => `/hubs/${hubId}/${colParent}/${docId}`))
-
-function collectionCreate(rootPath) {
-    return async (snap, context) => {
-        const documentData = snap.data()
-
-        if (typeof rootPath == 'function') {
-            rootPath = rootPath(context.params || {})
-        }
-    
-        if (!documentData) {
-            console.log(`"${rootPath}" no data`);
-            return null;
-        }
-
-        console.log("Incoming", rootPath, documentData);
-    
-        const hub = await firestore.doc(rootPath).get();
-    
-        const { subColName = "subscribers" } = hub.data() || {};
-    
-        const collectionPath = `${rootPath}/${subColName}`;
-    
-        const subscribersPromise = (() => {
-            if (documentData[subColName]) {
-                const docs = documentData[subColName].map(subscriberId => firestore.doc(`${collectionPath}/${subscriberId}`));
-                return firestore.getAll(...docs);
-            } else {
-                return firestore.collection(collectionPath).get().then(s => s.docs);
-            }
-        })();
-    
-        return subscribersPromise.then(async subscribers => {
-
-            console.log(`Sending to ${subscribers.length} subscribers`, subscribers.map(s => s.data()));
-    
-            return Promise.all(subscribers.map(async subscriber => {
-    
-                const subscriberData = subscriber.data();
-    
-                if (!subscriberData) {
-                    console.log(`"${subscriber.ref.path}" no data`)
-                    return null
-                }
-    
-                if (!subscriberData.tokens || !subscriberData.tokens.length) {
-                    console.log(`"${subscriber.ref.path}" no data.tokens`)
-                    return null
-                }
-    
-                return admin.messaging().sendToDevice(subscriberData.tokens, {
-                    data: {
-                        ...documentData.notification,
-                        ...documentData.text && {
-                            title: 'Incoming message',
-                            body: documentData.text 
-                        }
-                    }
-                });
-    
-            }));
-            
-        });
-    }
-}
